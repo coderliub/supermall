39b2068d23e68b6d2657ff6f65fb7751b9e6746e
coderliub <982498358@qq.com>
2022-06-18 15:47:40 +0800

    完成上拉加载更多

--- src/network/home.js
+++ src/network/home.js
@@ -5,3 +5,19 @@ export function getHomeMultidata() {
     url: '/home/multidata'
   })
 }
+
+export function getHomeGoods(type,page) {
+  return request({
+    url: 'home/data',
+    params: {
+      type,
+      page
+    }
+  })
+}
+// let totalNums = []
+//
+// const nums1 = [111,22,333]
+// const nums = [20,11,222]
+//
+// totalNums.push(...nums1)
